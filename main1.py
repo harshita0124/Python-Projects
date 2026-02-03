01ea8460e66fa915cf6a328798c6a0d41c8f80fe
Harshita Gupta <harshita.gupta3021@gmail.com>
2026-02-03 18:43:05 +0530

    Delete main1.py

--- main1.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-from PIL import Image
-
-array_image = np.random.randint(0, 256, (64, 64), dtype = np.uint8)
-
-image = Image.fromarray(array_image, mode = "L" )
-
-image.save("test2.png")
