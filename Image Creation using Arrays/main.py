499bdf31c1fe2e79532f153c03afad7966c8cd9f
Harshita Gupta <harshita.gupta3021@gmail.com>
2026-02-03 19:18:54 +0530

    Add files via upload

--- Image Creation using Arrays/main.py
+++ Image Creation using Arrays/main.py
@@ -1,1 +1,20 @@
+import numpy as np
+from PIL import Image
+
+# 0 -> black
+# 255 -> white
+
+array_image = np.array([[0, 0, 0, 0, 0, 0],
+                        [255, 255, 255, 255, 255, 255],
+                        [0, 0, 0, 255, 255, 255],
+                        [255, 255, 255, 0, 0, 0],
+                        [0, 0, 0, 0, 0, 0],
+                        [255, 255, 255, 255, 255, 255],
+                        [255, 255, 255, 255, 255, 0],
+                        [0, 0, 255, 255, 255, 255],
+                        [0, 0, 0, 0, 0, 0]], dtype = np.int32)
+
+image = Image.fromarray(array_image, mode = "L")
+
+image.save("test1.png")
 
